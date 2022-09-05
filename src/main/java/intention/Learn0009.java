88f21db762a727a7d7f56e685dde2fe93e7356c9
ligang2 <ligang2@liepin.com>
2022-09-05 10:21:14 +0800

    2022090501

--- src/main/java/intention/Learn0009.java
+++ src/main/java/intention/Learn0009.java
@@ -24,12 +24,57 @@ public class Learn0009 {
     }
 
     public void test02(){
-
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
+
+    /*
+    ****    4
+    *  *    2(中间有2个空格)
+    ****    4
+     */
+
+    public void test03(){
+        for (int i = 0; i < 3; i++) {
+            if (i == 0 || i == 2){
+                for (int j = 0; j < 5; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }else {
+                for (int j = 0; j < 5; j++) {
+                    if (j == 0 || j == 4){
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+        }
+    }
+
+    /*
+    等边三角形
+     *
+    ***
+   *****
+  *******
+     */
+
+    public
 
     public static void main(String[] args){
         Learn0009 learn0009 = new Learn0009();
-        learn0009.test01();
+        //learn0009.test01();
+        System.out.println("===============================");
+        //learn0009.test02();
+        System.out.println("===============================");
+        learn0009.test03();
     }
 
     
