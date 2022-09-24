8f0de335620ec51eb4b71241dc487d64be7fc686
ligang2 <ligang2@liepin.com>
2022-09-24 09:22:55 +0800

    2022092401

--- src/main/java/intention/Learn0009.java
+++ src/main/java/intention/Learn0009.java
@@ -60,13 +60,38 @@ public class Learn0009 {
 
     /*
     等边三角形
-     *
-    ***
-   *****
-  *******
+     *        1  2n-1
+    ***       3  2n-1
+   *****      5  2n-1
+  *******     7  2n-1
      */
 
-    public
+    public void test04(){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        /*
+         *        前面3个空格   第1行
+         ***      2个空格  第2行
+         *****    1个空格  第3行
+         *******  0个空格  第4行
+         */
+    }
+
+    public void test05(){
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 
     public static void main(String[] args){
         Learn0009 learn0009 = new Learn0009();
@@ -74,7 +99,9 @@ public class Learn0009 {
         System.out.println("===============================");
         //learn0009.test02();
         System.out.println("===============================");
-        learn0009.test03();
+        //learn0009.test03();
+        //learn0009.test04();
+        learn0009.test05();
     }
 
     
---
+++ src/main/java/intention/Learn0010.java
@@ -0,0 +1,13 @@
+package intention;
+
+public class Learn0010 {
+
+    private int age;
+    private String name;
+
+    //有参构造方法
+    public Learn0010(int age,String name){
+        this.age = age;
+        this.name = name;
+    }
+}
